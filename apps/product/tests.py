d8810d400b59305ea053acda43af90afe8d6e37c
saeed <saeedmouzarmi@gmail.com>
2025-04-21 19:09:47 +0330

    Added tests for product.

--- apps/product/tests.py
+++ apps/product/tests.py
@@ -1,3 +1,79 @@
 from django.test import TestCase
+from apps.product.models import Product, Category
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+from apps.users.models import User
+
+
+class ProductModelTests(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name='الکترونیکی',
+            description='دسته‌بندی محصولات الکترونیکی'
+        )
+
+    def test_create_product(self):
+        simple_product_name = "product test"
+        simple_product_description = "product test description"
+        product = Product.objects.create(
+            name=simple_product_name,
+            description='product description 1',
+            price=15000000,
+            category=self.category
+        )
+        self.assertEqual(product.name, simple_product_name)
+        self.assertEqual(product.category.name, simple_product_description)
+        self.assertEqual(product.price, 15000000)
+
+
+class ProductViewSetTests(APITestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            name='product test',
+            description='product test description',
+            price=15000000,
+        )
+
+        # Create users with different roles
+        self.admin = User.objects.create_superuser(
+            username='admin',
+            password='adminpass',
+            is_admin=True
+        )
+
+        self.customer = User.objects.create_user(
+            username='customer',
+            password='customerpass',
+            is_admin=False
+        )
+
+    def test_product_list(self):
+        url = reverse('products')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_product_create_as_admin(self):
+        self.client.force_authenticate(user=self.admin)
+        url = reverse('products')
+        data = {
+            'name': 'موبایل',
+            'price': 8000000,
+            'description': "product test description"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 2)
+
+    def test_product_create_as_customer(self):
+        self.client.force_authenticate(user=self.customer)
+        url = reverse('product-list')
+        data = {
+            'name': 'موبایل',
+            'price': 8000000,
+            'description': "product test description"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
