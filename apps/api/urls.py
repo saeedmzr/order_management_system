ff3ad71c35e3d64da37307e8b59a081a93fb55d0
saeed <saeedmouzarmi@gmail.com>
2025-04-21 17:02:41 +0330

    Added urls for swagger

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include(('apps.users.urls', 'users'))),
-    path("products/",include('apps.product.urls', 'product')),
-    path("orders/",include('apps.order.urls', 'order')),
+    path("products/",include(('apps.product.urls', 'product'))),
+    path("orders/",include(('apps.order.urls', 'order'))),
 ]
